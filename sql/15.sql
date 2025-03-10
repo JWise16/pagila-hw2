b275299ff6d9821b975c1ac738a54d5108cc5ae6
jonny wise <jonathanwise16@gmail.com>
2025-03-10 15:24:46 -0700

    fixed remaining problems

--- sql/15.sql
+++ sql/15.sql
@@ -1,10 +1,12 @@
 /*
  * Compute the total revenue for each film.
  */
-SELECT f.title, COALESCE(SUM(p.amount), 0)::numeric(10,2) AS revenue
-FROM film f
-LEFT JOIN inventory i ON i.film_id = f.film_id
-LEFT JOIN rental r ON i.inventory_id = r.inventory_id
-LEFT JOIN payment p ON r.rental_id = p.rental_id
-GROUP BY f.title
-ORDER BY revenue DESC;
+SELECT film.title, 
+       COALESCE(SUM(payment.amount), 0.00) AS revenue
+FROM film
+LEFT JOIN inventory USING (film_id)
+LEFT JOIN rental USING (inventory_id)
+LEFT JOIN payment USING (rental_id)
+GROUP BY film.title
+ORDER BY revenue DESC, film.title ASC;
+
--- sql/16.sql
+++ sql/16.sql
@@ -9,18 +9,17 @@
  * You can find examples of how to use the `rank` function at
  * <https://www.postgresqltutorial.com/postgresql-window-function/postgresql-rank-function/>.
  */
-SELECT
-    RANK() OVER (ORDER BY total_revenue DESC) AS rank,
-    title,
-    Total_revenue AS revenue
-  FROM (
-  SELECT
-      f.title,
-      COALESCE(SUM(p.amount), 0)::numeric(10,2) AS total_revenue
-  FROM film f
-  LEFT JOIN inventory i ON i.film_id = f.film_id
-  LEFT JOIN rental r ON i.inventory_id = r.inventory_id
-  LEFT JOIN payment p ON r.rental_id = p.rental_id
-  GROUP BY f.title
+SELECT RANK() OVER (ORDER BY revenue DESC) AS rank,
+       title,
+       revenue
+FROM (
+    SELECT film.title, 
+           COALESCE(SUM(payment.amount), 0.00) AS revenue
+    FROM film
+    LEFT JOIN inventory USING (film_id)
+    LEFT JOIN rental USING (inventory_id)
+    LEFT JOIN payment USING (rental_id)
+    GROUP BY film.title
 ) AS film_revenue
-ORDER BY rank;
+ORDER BY revenue DESC, title ASC;
+
